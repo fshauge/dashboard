9b74ccb506c93ad6e9d2f9efe6bc6827b2f24251
Markus Hauge <markus.hauge@hotmail.com>
2019-06-26 22:46:14 +0200

    Add simple cards to dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,19 +1,47 @@
 import React from "react";
 import styled from "styled-components";
+import Card from "./Card";
 
 const Grid = styled.main`
   flex: 1;
   display: grid;
   grid-template-columns: repeat(3, 1fr);
-  grid-auto-rows: auto;
+  grid-auto-rows: min-content;
+  flex-direction: column;
   justify-items: center;
+  gap: 2rem;
+  padding: 2rem;
 `;
 
 const Dashboard = () => (
   <Grid>
-    <h1>Column</h1>
-    <h1>Column</h1>
-    <h1>Column</h1>
+    <Card>
+      <h2>Card</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+        enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+        nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+        maiores amet?
+      </p>
+    </Card>
+    <Card>
+      <h2>Card</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+        enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+        nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+        maiores amet?
+      </p>
+    </Card>
+    <Card>
+      <h2>Card</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+        enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+        nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+        maiores amet?
+      </p>
+    </Card>
   </Grid>
 );
 
