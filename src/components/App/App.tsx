6a60cb6df84d5871fa559c53529addc74c9ca793
Markus Hauge <markus.hauge@hotmail.com>
2019-06-27 17:37:34 +0200

    Update styling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Dashboard from "../Dashboard";
 import styles from "./App.module.scss";
 
-const App = () => (
-  <div className={styles.grid}>
-    <div className={styles["status-overlay"]} />
-    <div className={styles["dashboard-container"]}>
+const App = () => {
+  const [opacity, setOpacity] = useState(0);
+
+  const handleScroll = () => {
+    setOpacity(document.documentElement.scrollTop / 100);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  return (
+    <div className={styles.container}>
+      <div style={{ opacity }} className={styles.statusOverlay} />
       <Dashboard />
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -2,36 +2,65 @@ import React from "react";
 import Card from "../Card";
 import styles from "./Dashboard.module.scss";
 
-const Dashboard = () => (
-  <div className={styles.grid}>
-    <Card>
-      <h2>Card</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
-        enim pariatur accusamus veniam exercitationem esse voluptas quisquam
-        nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
-        maiores amet?
-      </p>
-    </Card>
-    <Card>
-      <h2>Card</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
-        enim pariatur accusamus veniam exercitationem esse voluptas quisquam
-        nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
-        maiores amet?
-      </p>
-    </Card>
-    <Card>
-      <h2>Card</h2>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
-        enim pariatur accusamus veniam exercitationem esse voluptas quisquam
-        nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
-        maiores amet?
-      </p>
-    </Card>
-  </div>
-);
+const Dashboard = () => {
+  return (
+    <div className={styles.grid}>
+      <Card>
+        <h2>Card</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+          nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+          maiores amet?
+        </p>
+      </Card>
+      <Card>
+        <h2>Card</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+          nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+          maiores amet?
+        </p>
+      </Card>
+      <Card>
+        <h2>Card</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+          nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+          maiores amet?
+        </p>
+      </Card>
+      <Card>
+        <h2>Card</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+          nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+          maiores amet?
+        </p>
+      </Card>
+      <Card>
+        <h2>Card</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+          nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+          maiores amet?
+        </p>
+      </Card>
+      <Card>
+        <h2>Card</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          enim pariatur accusamus veniam exercitationem esse voluptas quisquam
+          nisi, rem fugit, numquam, velit dicta! Soluta impedit ex tempora nemo
+          maiores amet?
+        </p>
+      </Card>
+    </div>
+  );
+};
 
 export default Dashboard;
